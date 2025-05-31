b0b096ac2d1e5e4b7cb00e2c9e86989ac7471083
hamza <hamzahmamo36@gmail.com>
2025-05-31 20:47:21 +0100

    rj3te versuib l9dina

--- src/main/java/net/hamza/banque/security/StarterSecurity.java
+++
@@ -1,9 +0,0 @@
-package net.hamza.banque.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-@Configuration
-@EnableWebSecurity
-public class StarterSecurity extends AbstractSecurityWebApplicationInitializer {
-}
